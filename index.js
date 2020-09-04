788855cece9ed2a9f858fecb980cf6fc7f90397a
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2020-09-04 16:51:59 +0700

    Learn done task 31 - NPM

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-console.log('Learn JS of Coder.Tokyosi');
-console.log('Learn JS of Coder.Tokyosi');
-console.log('Learn JS of Coder.Tokyosi');
+var reallineSync = require('readline-sync');
 
-console.log('Hoang Toni');
+// var userName = reallineSync.question('May I your name?');
+// console.log('Hi ' + userName + ' !');
 
+// var language = [];
+// var languageMother = reallineSync.question('Which your mother language ? ');
+// console.log(languageMother);
+
+// language.push(languageMother);
+// console.log(language);
+
+var pet = {};
+
+var name = reallineSync.question('What is your pet name: ');
+pet.name = name;
+
+var weight = reallineSync.question('Weight: ', {
+    hideEchoBack: true,
+});
+pet.weight = Number(weight);
+
+var color = reallineSync.question('Color: ', {
+    hideEchoBack: !false,
+    
+});
+pet.color = color;
+
+console.log(pet);
